b1c767c5b201ae5f81a18d92e5372020af971af9
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2025-10-12 20:43:00 +0300

    added complete assets folder to discord bot page

---
+++ assets/js/misc.js
@@ -0,0 +1,6 @@
+// Miscellaneous Scripts
+
+added new easter egg JS imports in all main site pages
+removed all easter egg scripts from misc.js file
+added dedicated JS file for radio easter egg
+added dedicated JS file for go-to easter egg
